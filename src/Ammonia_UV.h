f3375a8182e6502d03296ba75c844f8e13781281
evncrl <carlevan.piad05@gmail.com>
2026-06-17 19:04:34 +0800

    coms

--- src/Ammonia_UV.h
+++ src/Ammonia_UV.h
@@ -12,16 +12,12 @@ private:
     float _estimatedVoltage;
 
 public:
-    // Constructor upang i-initialize ang mga pins at threshold
     AmmoniaUV(int mq137Pin, int relayPin, int threshold);
 
-    // I-setup ang mga pin modes
     void begin();
 
-    // Babasahin ang sensor at ikokontrol ang relay
     void update();
 
-    // Getters kung sakaling kailanganin mo ang data sa ibang bahagi ng code
     int getRawAmmonia();
     float getVoltage();
 };
